f834b1793d8b27fa5ceadf357ae37c2266570f0d
maximushugus <36080343+maximushugus@users.noreply.github.com>
2022-03-19 23:15:11 +0100

    Update clusters.py

--- zigate/clusters.py
+++ zigate/clusters.py
@@ -627,7 +627,7 @@ class C0500(Cluster):
 class C0702(Cluster):
     cluster_id = 0x0702
     type = 'Metering'
-    attributes_def = {0x0000: {'name': 'index_base', 'value': "int('value', 16)",
+    attributes_def = {0x0000: {'name': 'index_base', 'value': 'value',
                                'unit': 'Wh', 'type': int},
                       0x0400: {'name': 'instant_power', 'value': 'value',
                                'type': float},
