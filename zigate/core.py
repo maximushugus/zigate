0fe31f51b59d9ec1bed99843ca699f9f8148e9d4
maximushugus <36080343+maximushugus@users.noreply.github.com>
2022-09-11 17:10:05 +0200

    Update core.py

--- zigate/core.py
+++ zigate/core.py
@@ -2700,16 +2700,16 @@ class Device(object):
                 self._zigate.bind_addr(self.addr, endpoint_id, 0x0702)
                 self._zigate.reporting_request(self.addr,
                                                endpoint_id,
-                                               0x0702, (0x0000, 0x29))
+                                               0x0702, (0x0000, 0x25))
             if 0x0B04 in endpoint['in_clusters']:
                 LOGGER.debug('bind for cluster 0x0B04')
                 self._zigate.bind_addr(self.addr, endpoint_id, 0x0B04)
                 self._zigate.reporting_request(self.addr,
                                                endpoint_id,
-                                               0x0B04, (0x0508, 0x29))
+                                               0x0B04, (0x0508, 0x21))
                 self._zigate.reporting_request(self.addr,
                                                endpoint_id,
-                                               0x0B04, (0x050F, 0x29))
+                                               0x0B04, (0x050F, 0x21))
 
     @staticmethod
     def from_json(data, zigate_instance=None):
